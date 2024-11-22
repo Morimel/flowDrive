b383fc388d29df18ce1494c87b79684fd255f8e8
Morimel <melsovisa77@gmail.com>
2024-11-22 08:18:04 +0600

    push

--- flowDrive/settings.py
+++ flowDrive/settings.py
@@ -67,6 +67,7 @@ EMAIL_HOST_PASSWORD = 'iuwh hgpf xhnq ujkg'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,6 +143,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage",
 
 # Папка для загрузки файлов
 MEDIA_URL = '/media/'
